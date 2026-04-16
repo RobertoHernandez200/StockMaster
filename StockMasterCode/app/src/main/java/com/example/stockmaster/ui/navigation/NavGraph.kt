6cc418456688a344d966220e334eef7869d3dbf2
RobertoHernandez200 <rocarhero@gmail.com>
2026-04-16 01:21:29 -0500

    sas

--- StockMasterCode/app/src/main/java/com/example/stockmaster/ui/navigation/NavGraph.kt
+++ StockMasterCode/app/src/main/java/com/example/stockmaster/ui/navigation/NavGraph.kt
@@ -2,18 +2,20 @@ package com.example.stockmaster.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.*
+import com.example.stockmaster.ui.screens.home_cliente.HomeClienteScreen
 import com.example.stockmaster.ui.screens.home_tienda.HomeTiendaScreen
 import com.example.stockmaster.ui.screens.products.ProductScreen
-// importa lo demás igual que antes
 
 @Composable
 fun NavGraph() {
 
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "splash") {
+    NavHost(navController = navController, startDestination = "home_tienda") {
 
-        // todo lo anterior igual...
+        composable("home_cliente") {
+            HomeClienteScreen()
+        }
 
         composable("home_tienda") {
             HomeTiendaScreen(
--- StockMasterCode/app/src/main/java/com/example/stockmaster/ui/screens/products/ProductScreen.kt
+++ StockMasterCode/app/src/main/java/com/example/stockmaster/ui/screens/products/ProductScreen.kt
@@ -1,9 +1,19 @@
 package com.example.stockmaster.ui.screens.products
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ProductScreen() {
-    Text("Hola Producto 🧾")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Pantalla de Productos 📦")
+    }
 }
