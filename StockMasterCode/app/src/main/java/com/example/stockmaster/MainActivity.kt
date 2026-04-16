4fa0648576119eeb0769d5ca786ff82d7dd5ffc3
RobertoHernandez200 <rocarhero@gmail.com>
2026-04-15 21:58:17 -0500

    prueba2

--- StockMasterCode/app/src/main/java/com/example/stockmaster/MainActivity.kt
+++ StockMasterCode/app/src/main/java/com/example/stockmaster/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hola $name!",
+        text = "Hola bro $name!",
         modifier = modifier
     )
 }
