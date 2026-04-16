d574d1b8018a2f9005f7500c26fb1476727b3458
RobertoHernandez200 <rocarhero@gmail.com>
2026-04-16 16:14:32 -0500

    siete

--- StockMasterCode/app/src/main/java/com/example/stockmaster/ui/components/PrimaryButton.kt
+++ StockMasterCode/app/src/main/java/com/example/stockmaster/ui/components/PrimaryButton.kt
@@ -1,44 +1,20 @@
 package com.example.stockmaster.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.clickable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun PrimaryButton(
     text: String,
     onClick: () -> Unit
 ) {
-
-    val gradient = Brush.horizontalGradient(
-        colors = listOf(
-            Color(0xFF6A5AE0),
-            Color(0xFF8E7CFF)
-        )
-    )
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(55.dp)
-            .clip(RoundedCornerShape(30.dp))
-            .background(gradient)
-            .clickable { onClick() },
-        contentAlignment = Alignment.Center
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Text(
-            text = text,
-            color = Color.White,
-            fontSize = 16.sp
-        )
+        Text(text = text)
     }
 }
